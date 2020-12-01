8effe2c3a7e40a977a7845851fed3e9c7218a61b
Mathias Staricka <mathias@staricka.com>
2020-11-30 21:25:01 -0800

    day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,9 +1,27 @@
-pub fn part1() {
-    println!("1:1");
+pub fn part1(input: &str) -> String {
+    let list = input.split_whitespace().map(|s| s.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    for i in 0..list.len() {
+        for j in 0..list.len() {
+            if list[i] + list[j] == 2020 {
+                return (list[i] * list[j]).to_string();
+            }
+        }
+    }
+    String::new()
 }
 
-pub fn part2() {
-    println!("1:2");
+pub fn part2(input: &str) -> String {
+    let list = input.split_whitespace().map(|s| s.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    for i in 0..list.len() {
+        for j in 0..list.len() {
+            for k in 0..list.len() {
+                if list[i] + list[j] + list[k] == 2020 {
+                    return (list[i] * list[j] * list[k]).to_string();
+                }
+            }
+        }
+    }
+    String::new()
 }
 
 #[cfg(test)]
@@ -12,6 +30,21 @@ mod tests {
 
     #[test]
     fn part1_test1() {
-        panic!("Not implemented")
+        assert_eq!(part1("1721
+979
+366
+299
+675
+1456"), "514579");
+    }
+
+    #[test]
+    fn part2_test1() {
+        assert_eq!(part2("1721
+979
+366
+299
+675
+1456"), "241861950");
     }
 }
