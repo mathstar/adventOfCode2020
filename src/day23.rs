644960a171f17b0fe295bcad9d5c2f5f2fca92a3
Mathias Staricka <mstaricka@gmail.com>
2021-11-02 14:45:32 -0700

    day 23 - reimplemented to be much more efficient, solved part 2

--- src/day23.rs
+++ src/day23.rs
@@ -1,67 +1,150 @@
 use crate::day::Day;
-use std::cmp::min;
 
 pub struct Day23 {
     pub move_count: u32
 }
 
-fn find_cup(cup: u32, cups: &Vec<u32>) -> usize {
-    cups.iter().enumerate().filter(|(_,c)| **c == cup).next().unwrap().0
+#[allow(dead_code)]
+fn pretty_print(cups: &Vec<usize>, current: usize) -> String {
+    let mut result;
+    if current == 1 {
+        result = "(1),".to_owned();
+    } else {
+        result = "1,".to_owned();
+    }
+    let mut cup = cups[1];
+    while cup != 1 {
+        if current == cup {
+            result += format!("({}),", cup).as_str();
+        } else {
+            result += format!("{},", cup).as_str();
+        }
+        cup = cups[cup];
+    }
+    result
 }
 
 impl Day for Day23 {
     fn part1(&self, input: &str) -> String {
-        let mut cups = input.lines().next().unwrap().chars().map(|c| c as u32 - '0' as u32).collect::<Vec<u32>>();
-        let mut current_cup = cups[0];
-        let min_cup = *cups.iter().min().unwrap();
-        let max_cup = *cups.iter().max().unwrap();
+        let input = input.lines().next().unwrap();
+        let mut input_iter = input.chars().map(|c| c as usize - '0' as usize);
 
+        // adjacency vector - cups[i] = <cup clockwise from cup i>
+        let mut cups = vec![0usize; input.len() + 1];
+
+        // generate initial cup
+        let mut current = input_iter.next().unwrap();
+        let mut prev = current;
+        cups[current] = current;
+
+        // insert cups from input
+        for val in input_iter {
+            cups[prev] = val;
+            prev = val;
+            cups[val] = current;
+        }
+
+        // perform moves
         for _ in 0..self.move_count {
-            let current_cup_index = find_cup(current_cup, &cups);
-            let next_cup_index = current_cup_index + 1;
-            let cup0 = if next_cup_index < cups.len() {cups.remove(next_cup_index)} else {cups.remove(0)};
-            let cup1 = if next_cup_index < cups.len() {cups.remove(next_cup_index)} else {cups.remove(0)};
-            let cup2 = if next_cup_index < cups.len() {cups.remove(next_cup_index)} else {cups.remove(0)};
+            // identify cups to "pick up"
+            let next_1 = cups[current];
+            let next_2 = cups[next_1];
+            let next_3 = cups[next_2];
 
-            let mut destination_cup = current_cup - 1;
-            if destination_cup < min_cup { destination_cup = max_cup; }
-            while destination_cup == cup0 || destination_cup == cup1 || destination_cup == cup2 {
-                destination_cup -= 1;
-                if destination_cup < min_cup {
-                    destination_cup = max_cup;
+            // "remove" cups by pointing current's next at cup after picked up cups
+            cups[current] = cups[next_3];
+
+            // identify destination
+            let mut destination = current - 1;
+            if destination < 1 {
+                destination = cups.len() - 1;
+            }
+            while destination == next_1 || destination == next_2 || destination == next_3 {
+                destination -= 1;
+                if destination < 1 {
+                    destination = cups.len() - 1;
                 }
             }
-            let mut destination_index = find_cup(destination_cup, &cups) + 1;
-            if destination_index > cups.len() {
-                cups.push(cup0);
-                cups.push(cup1);
-                cups.push(cup2);
-            } else {
-                cups.insert(destination_index, cup0);
-                cups.insert(destination_index + 1, cup1);
-                cups.insert(destination_index + 2, cup2);
 
-            }
+            // insert cups after destination
+            let after_destination = cups[destination];
+            cups[destination] = next_1;
+            cups[next_3] = after_destination;
 
-            let next_current_cup_index = find_cup(current_cup, &cups) + 1;
-            current_cup = if next_current_cup_index < cups.len() {cups[next_current_cup_index]} else {cups[0]}
+            // pick new current cup
+            current = cups[current];
         }
 
         let mut result = "".to_owned();
-        let mut initial = find_cup(1, &cups);
-        let mut i = initial + 1;
-        if i >= cups.len() {i = 0};
-        while i != initial {
-            result += cups[i].to_string().as_str();
-            i += 1;
-            if i >= cups.len() {i = 0};
+        let mut next = cups[1];
+        while next != 1 {
+            result += next.to_string().as_str();
+            next = cups[next];
         }
         result
     }
 
     fn part2(&self, input: &str) -> String {
+        let mut input_iter = input.lines().next().unwrap().chars().map(|c| c as usize - '0' as usize);
 
-        String::new()
+        // adjacency vector - cups[i] = <cup clockwise from cup i>
+        let mut cups = vec![0usize; 1000001];
+
+        // generate initial cup
+        let mut current = input_iter.next().unwrap();
+        let mut prev = current;
+        cups[current] = current;
+
+        // insert cups from input
+        let mut max = current;
+        for val in input_iter {
+            cups[prev] = val;
+            prev = val;
+            cups[val] = current;
+
+            max = std::cmp::max(max, val);
+        }
+
+        // insert remaining cups
+        for val in max+1..1000001 {
+            cups[prev] = val;
+            prev = val;
+            cups[val] = current;
+        }
+
+        // perform moves
+        for _ in 0..10000000 {
+            // identify cups to "pick up"
+            let next_1 = cups[current];
+            let next_2 = cups[next_1];
+            let next_3 = cups[next_2];
+
+            // "remove" cups by pointing current's next at cup after picked up cups
+            cups[current] = cups[next_3];
+
+            // identify destination
+            let mut destination = current - 1;
+            if destination < 1 {
+                destination = 1000000;
+            }
+            while destination == next_1 || destination == next_2 || destination == next_3 {
+                destination -= 1;
+                if destination < 1 {
+                    destination = 1000000;
+                }
+            }
+
+            // insert cups after destination
+            let after_destination = cups[destination];
+            cups[destination] = next_1;
+            cups[next_3] = after_destination;
+
+            // pick new current cup
+            current = cups[current];
+        }
+
+        let result = cups[1];
+        (result as u64 * cups[result] as u64).to_string()
     }
 }
 
@@ -75,7 +158,12 @@ mod tests {
     }
 
     #[test]
+    fn part1_test2() {
+        assert_eq!(Day23{move_count: 100}.part1("389125467"), "67384529");
+    }
+
+    #[test]
     fn part2_test1() {
-        assert_eq!(Day23{move_count: 10}.part2(""), "");
+        assert_eq!(Day23{move_count: 10000000}.part2("389125467"), "149245887792");
     }
 }
